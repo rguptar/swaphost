2d498b02ea6a68ed5022139db4cf6a624d49cadc
Rahul Gupta <guptar@microsoft.com>
2023-03-01 19:02:07 +0000

    skip validation flag

--- swaphost.sh
+++ swaphost.sh
@@ -3,7 +3,15 @@
 source ./semver2.sh
 
 function usage {
-    echo "Usage: $0 -g <resource group> -n <cluster name> -m <mode> [-p <priority>] -s <new vm sku> -a <old nodepool> -b <new nodepool>"
+    echo "Usage: $0 -g <resource group> -n <cluster name> -m <mode> [-p <priority>] -s <new vm sku> -a <old nodepool> -b <new nodepool> [-f]"
+    echo "  -g <resource group>     resource group of the cluster"
+    echo "  -n <cluster name>       name of the cluster"
+    echo "  -m <mode>               mode of the new nodepool; must match the old nodepool {System, User}"
+    echo "  -p <priority>           priority of the new nodepool {Regular, Spot}"
+    echo "  -s <new vm sku>         new vm sku"
+    echo "  -a <old nodepool>       name of the old nodepool"
+    echo "  -b <new nodepool>       name of the new nodepool"
+    echo "  -f                      skip validation"
     exit
 }
 
@@ -22,13 +30,17 @@ newNodepool=
 newVmSku=
 mode=
 priority=Regular
-args=$(getopt ha:b:g:m:n:p:s: $*)
+skipValidation=0
+args=$(getopt hfa:b:g:m:n:p:s: $*)
 set -- $args
 for i; do
     case "$i" in
     -h)
         usage
         exit
+        ;;
+    -f)
+        skipValidation=1
         ;;
     -g)
         rg=$2
@@ -86,68 +98,69 @@ function invalid() {
     exit
 }
 
-if [ "$mode" != "System" ] && [ "$mode" != "User" ]; then
-    invalid "invalid mode $mode"
-fi
-valid "valid mode $mode"
-
-if [ "$priority" != "Regular" ] && [ "$priority" != "Spot" ]; then
-    invalid "invalid priority $priority"
-fi
-# todo: mark System + Spot combo as invalid
-valid "valid priority $priority"
-
-# todo: add --no-validate flag
-oldNodepoolExists=0
-oldNodepoolMode=
-nodepools=$(az aks show -g $rg -n $cluster | jq '.agentPoolProfiles[] | {name: .name, mode: .mode}' | jq -s)
-for nodepool in $(get_json_array "$nodepools" ".[]"); do
-    name=$(echo $(get_json_array "$nodepool" ".name"))
-    if [ "$name" == "$oldNodepool" ]; then
-        oldNodepoolExists=1
-        oldNodepoolMode=$(echo $(get_json_array "$nodepool" ".mode"))
-        valid "old nodepool exists"
+if [ $skipValidation -eq 0 ]; then
+    if [ "$mode" != "System" ] && [ "$mode" != "User" ]; then
+        invalid "invalid mode $mode"
     fi
-    if [ "$name" == "$newNodepool" ]; then
-        invalid "new nodepool exists"
+    valid "valid mode $mode"
+
+    if [ "$priority" != "Regular" ] && [ "$priority" != "Spot" ]; then
+        invalid "invalid priority $priority"
     fi
-done
-if [ $oldNodepoolExists -eq 0 ]; then
-    invalid "old nodepool does not exist"
-fi
-valid "new nodepool does not exist"
-# todo: add support for same name
+    # todo: mark System + Spot combo as invalid
+    valid "valid priority $priority"
 
-if [ "$oldNodepoolMode" != "$mode" ]; then
-    invalid "old nodepool mode $oldNodepoolMode does not match new nodepool mode $mode"
-fi
-valid "modes match"
-
-aksLocation=$(az aks show -g $rg -n $cluster --query location -o tsv)
-skuExists=$(az vm list-sizes --location $aksLocation --query "[].name" | jq -r "index(\"$newVmSku\")")
-if [ "$skuExists" == "null" ]; then
-    invalid "the specified sku $newVmSku does not exist in location $aksLocation"
-fi
-valid "sku valid"
-
-clusterVersion=$(az aks show -g $rg -n $cluster --query kubernetesVersion -o tsv)
-comparison=$(semver_compare "$clusterVersion" "1.21")
-if [ $comparison -lt 0 ]; then
-    invalid "pod disruption budget is not available for k8s versions older than 1.21; cluster $cluster runs on $clusterVersion"
-fi
-valid "pdb supported"
-
-# todo: filter pdb by workloads running on the specified old nodepool
-az aks get-credentials -g $rg -n $cluster
-pdbs=$(kubectl get pdb -A -o json | jq)
-for pdb in $(get_json_array "$pdbs" ".items[]"); do
-    name=$(get_json_value "$pdb" '.metadata.name')
-    disruptionsAllowed=$(get_json_value "$pdb" '.status.disruptionsAllowed')
-    if [ "$disruptionsAllowed" -lt 1 ]; then
-        invalid "pdb is not sufficient for evicting pods: $name; try again later"
+    oldNodepoolExists=0
+    oldNodepoolMode=
+    nodepools=$(az aks show -g $rg -n $cluster | jq '.agentPoolProfiles[] | {name: .name, mode: .mode}' | jq -s)
+    for nodepool in $(get_json_array "$nodepools" ".[]"); do
+        name=$(echo $(get_json_array "$nodepool" ".name"))
+        if [ "$name" == "$oldNodepool" ]; then
+            oldNodepoolExists=1
+            oldNodepoolMode=$(echo $(get_json_array "$nodepool" ".mode"))
+            valid "old nodepool exists"
+        fi
+        if [ "$name" == "$newNodepool" ]; then
+            invalid "new nodepool exists"
+        fi
+    done
+    if [ $oldNodepoolExists -eq 0 ]; then
+        invalid "old nodepool does not exist"
     fi
-done
-valid "pdb found"
+    valid "new nodepool does not exist"
+    # todo: add support for same name
+
+    if [ "$oldNodepoolMode" != "$mode" ]; then
+        invalid "old nodepool mode $oldNodepoolMode does not match new nodepool mode $mode"
+    fi
+    valid "modes match"
+
+    aksLocation=$(az aks show -g $rg -n $cluster --query location -o tsv)
+    skuExists=$(az vm list-sizes --location $aksLocation --query "[].name" | jq -r "index(\"$newVmSku\")")
+    if [ "$skuExists" == "null" ]; then
+        invalid "the specified sku $newVmSku does not exist in location $aksLocation"
+    fi
+    valid "sku valid"
+
+    clusterVersion=$(az aks show -g $rg -n $cluster --query kubernetesVersion -o tsv)
+    comparison=$(semver_compare "$clusterVersion" "1.21")
+    if [ $comparison -lt 0 ]; then
+        invalid "pod disruption budget is not available for k8s versions older than 1.21; cluster $cluster runs on $clusterVersion"
+    fi
+    valid "pdb supported"
+
+    # todo: filter pdb by workloads running on the specified old nodepool
+    az aks get-credentials -g $rg -n $cluster
+    pdbs=$(kubectl get pdb -A -o json | jq)
+    for pdb in $(get_json_array "$pdbs" ".items[]"); do
+        name=$(get_json_value "$pdb" '.metadata.name')
+        disruptionsAllowed=$(get_json_value "$pdb" '.status.disruptionsAllowed')
+        if [ "$disruptionsAllowed" -lt 1 ]; then
+            invalid "pdb is not sufficient for evicting pods: $name; try again later"
+        fi
+    done
+    valid "pdb found"
+fi
 
 oldNodes=$(kubectl get nodes -o json | jq ".items[] | select(.metadata.labels.agentpool==\"$oldNodepool\") | {agentpool: .metadata.labels.agentpool, name: .metadata.name}" | jq -s)
 oldNodeCount=$(echo $oldNodes | jq "length")
