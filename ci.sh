30a444493010d1d57500a34c64bbc8156dbc3bce
Rahul Gupta <guptar@microsoft.com>
2023-03-07 09:10:33 -0800

    no validate

--- ci.sh
+++ ci.sh
@@ -1,3 +1,3 @@
 echo "ci"
 az login --service-principal -u $AZURE_USER -p $AZURE_PASSWORD --tenant $AZURE_TENANT --output none
-./swaphost.sh -g aks -n k8s -m System -p Regular -s Standard_D2s_v3 -a system -b sys
+./swaphost.sh -g aks -n k8s -m System -p Regular -s Standard_D2s_v3 -a system -b sys -f
